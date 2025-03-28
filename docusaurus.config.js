8383c68335f7ce587a6659bb89b4a7f2c33c918c
silvioviscuso <applab.staff@gmail.com>
2025-03-28 21:57:12 +0100

    fix path doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
             items: [
               {
                 label: 'Schematics',
-                to: '/docs/cover-block',
+                to: '/docs/schematic/cover-block',
               },
              
             ],
